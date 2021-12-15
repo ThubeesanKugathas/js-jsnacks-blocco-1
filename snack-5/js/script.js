c287120ab35620d8fbb3a3ae9bfa9676a77f0ce7
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-15 13:55:32 +0100

    snack 5 - to do!

---
+++ snack-5/js/script.js
@@ -0,0 +1,16 @@
+// snack-5 Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la
+// somma degli elementi è minore di 50.
+
+const array = [];
+let sum = 0;
+
+do {
+    const number = prompt('inserisci un numero');
+    sum += parseInt(number); 
+
+    console.log('La somma non è ancora 50');
+
+} while (sum < 50)
+
+console.log('la somma ha raggiunto o superato 50');
